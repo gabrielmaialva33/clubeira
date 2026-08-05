f3a7e901f980f65a2133453191e1c3bf958848d3
Gabriel Maia <gabrielmaialva33@gmail.com>
2026-08-05 11:53:27 -0300

    test(events): pass required attributes in emit transaction assertion

--- test/clubeira/events_test.exs
+++ test/clubeira/events_test.exs
@@ -5,8 +5,20 @@ defmodule Clubeira.EventsTest do
   alias Clubeira.Repo
 
   test "requires the event and outbox message to be emitted inside one transaction" do
+    attributes = %{
+      polo_id: Ecto.UUID.generate(version: 7),
+      aggregate_type: "test",
+      aggregate_id: Ecto.UUID.generate(version: 7),
+      aggregate_version: 1,
+      event_type: "test.emitted",
+      topic: "test.events",
+      message_key: "test",
+      payload: %{},
+      occurred_at: DateTime.utc_now(:microsecond)
+    }
+
     assert_raise ArgumentError,
                  "domain events must be emitted inside a database transaction",
-                 fn -> Events.emit!(Repo, %{}) end
+                 fn -> Events.emit!(Repo, attributes) end
   end
 end
