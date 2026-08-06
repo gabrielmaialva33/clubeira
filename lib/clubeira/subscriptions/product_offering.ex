dbd5aa85c28adb8e24f1e8810c990565845d5968
Gabriel Maia <gabrielmaialva33@gmail.com>
2026-08-06 14:56:00 -0300

    feat(db): add revision tracking column to product offerings

--- lib/clubeira/subscriptions/product_offering.ex
+++ lib/clubeira/subscriptions/product_offering.ex
@@ -17,7 +17,22 @@ defmodule Clubeira.Subscriptions.ProductOffering do
     field :scope_kind, :string
     field :sales_channel, :string
     field :status, :string
+    field :revision, :integer, default: 1
 
     timestamps()
   end
+
+  @type t :: %__MODULE__{
+          id: Ecto.UUID.t(),
+          polo_id: Ecto.UUID.t(),
+          access_product_version_id: Ecto.UUID.t(),
+          edition_id: Ecto.UUID.t() | nil,
+          code: String.t(),
+          scope_kind: String.t(),
+          sales_channel: String.t(),
+          status: String.t(),
+          revision: pos_integer(),
+          inserted_at: DateTime.t(),
+          updated_at: DateTime.t()
+        }
 end
